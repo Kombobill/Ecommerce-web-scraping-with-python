aa2dea10827c25dcc554ebbd01366c5a2d1a74c4
Kombobill <billmadru25@gmail.com>
2024-10-06 23:06:25 +0300

    import statements

---
+++ whisky.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+baseurl = 'https://www.thewhiskyexchange.com/'
+headers = {
+    'User-Agent': 'https://explore.whatismybrowser.com/useragents/parse/835464209-chrome-linux-blink'
+}
